c39bdfc6eed18558c04db66874dea3a04b8e9ca2
K <kenneyhe@users.noreply.github.com>
2023-03-13 14:41:26 -0700

    Openssl support
    add package installation

--- set-cross-compile.sh
+++ set-cross-compile.sh
@@ -13,6 +13,7 @@ if uname -a | grep --quiet --extended-regexp -i "darwin|msys|windows"; then
 fi
 
 if echo "$TARGET" | grep --quiet --extended-regexp -i '(x86_64|586|686).+linux-(gnu|musl)'; then
+    sudo apt-get install libssl-dev -y || true
     echo "needs-cross=false" >> $GITHUB_OUTPUT
     exit 0
 fi
