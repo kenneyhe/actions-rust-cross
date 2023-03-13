33b1bc8a04ca28c9fb0e19a665e9c30a06220a7c
K <kenneyhe@users.noreply.github.com>
2023-03-13 13:07:54 -0700

    Path support
    Default to have OPENSSL_DIR below:
    export PATH=.:$PATH
    export OPENSSL_DIR=/usr/include/openssl

--- install-cross-nix.sh
+++ install-cross-nix.sh
@@ -3,6 +3,8 @@ set -x
 
 cd "$RUNNER_TEMP"
 export TARGET=.
+export PATH=.:$PATH
+export OPENSSL_DIR=/usr/include/openssl
 curl --silent --location \
      https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
     sh
