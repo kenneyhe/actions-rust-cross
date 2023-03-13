5e2a373e5dbe20d8fd7f16c94126bb95a7f91d86
K <kenneyhe@users.noreply.github.com>
2023-03-13 14:50:19 -0700

    FIX: raspi
    Move openssl library installation

--- set-cross-compile.sh
+++ set-cross-compile.sh
@@ -4,7 +4,7 @@ set -x
 TARGET="$1"
 export PATH=.:$PATH
 export OPENSSL_DIR=/usr/include/openssl
-
+apt-get install libssl-dev -y || true
 # On macOS and Windows, we can cross-compile to all possible targets without
 # using cross.
 if uname -a | grep --quiet --extended-regexp -i "darwin|msys|windows"; then
@@ -13,7 +13,6 @@ if uname -a | grep --quiet --extended-regexp -i "darwin|msys|windows"; then
 fi
 
 if echo "$TARGET" | grep --quiet --extended-regexp -i '(x86_64|586|686).+linux-(gnu|musl)'; then
-    sudo apt-get install libssl-dev -y || true
     echo "needs-cross=false" >> $GITHUB_OUTPUT
     exit 0
 fi
